11d6c6a995b6cc7610810b641db572ab3c921fd9
Sabiha Islam <munshat.cuet16@gmail.com>
2022-02-08 01:06:09 +0600

    using new method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,56 +1,153 @@
 package com.company;
 import java.util.*;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.lang.Object;
 
-class product{
-    private String prodname;
-    private double bprice;
-    private double sprice;
-    private int availnum = 0;
-    private double profit = 0.0;
-
-    product(String prodname, double bprice, double sprice, int availnum, double profit){
-        this.prodname = prodname;
-        this.bprice = bprice;
-        this.sprice = sprice;
-        this.availnum = availnum;
-        this.profit = profit;
-    }
-
-    public String getProdname(){
-        return prodname;
-    }
-    public double getBprice(){
-        return bprice;
-    }
-
-    public double getSprice(){
-        return sprice;
-    }
-
-    public int getAvailnum(){
-        return availnum;
-    }
-
-    public double getProfit(){
-        return profit;
-    }
-
-    public String toString(){
-        return prodname+" "+bprice+" "+sprice+" "+availnum+" "+profit;
-    }
-}
 public class Main {
 
+    private static List<Map> product = new ArrayList<>();
     public static double balance = 20000;
+
+    static void prodAdd(){
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+
+        String pname;
+        double bp;
+        double sp;
+        int anum = 0;
+        double profit = 0.0;
+        boolean deleted = false;
+
+        System.out.println("Enter product name: ");
+        pname = s1.nextLine();
+        System.out.println("Enter buy price: ");
+        bp = s.nextDouble();
+        System.out.println("Enter sell price: ");
+        sp = s.nextDouble();
+
+        Map prod = new HashMap();
+        prod.put("pname", pname);
+        prod.put("bp", bp);
+        prod.put("sp", sp);
+        prod.put("anum", anum);
+        prod.put("profit", profit);
+        prod.put("deleted", deleted);
+
+        product.add(prod);
+        System.out.println(pname + " is added in the list.");
+        System.out.println("");
+    }
+    static void prodShow(){
+        double tprofit = 0.0;
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.printf("%10s %20s %10s", "Product Name", "Available Product", "Profit");
+        System.out.println();
+        System.out.println("-----------------------------------------------------------------------------");
+        for(int i = 0; i<product.size();i++){
+            tprofit += (double) product.get(i).get("profit");
+            if ((boolean) product.get(i).get("deleted"))
+                continue;
+            System.out.format("%10s %15d %17.2f",
+                    product.get(i).get("pname"), product.get(i).get("anum"), product.get(i).get("profit"));
+            System.out.println();
+        }
+        System.out.println("-----------------------------------------------------------------------------");
+        System.out.println("Total Profit: " + tprofit);
+        System.out.println("");
+    }
+    static void balance(){
+        System.out.println("");
+        System.out.println("Available balance: " + balance);
+        System.out.println("");
+    }
+    static void prodDelete() {
+        Scanner s = new Scanner(System.in);
+        boolean found = false;
+        System.out.print("Enter product name to delete :");
+        String delete = s.nextLine();
+        System.out.println("----------------------------");
+        for (int i = 0; i < product.size(); i++) {
+            String name = product.get(i).get("pname").toString();
+            boolean deleted = (boolean) product.get(i).get("deleted");
+            if (name.equals(delete) && !deleted) {
+                product.get(i).put("deleted", true);
+                found = true;
+                System.out.println(delete + "is deleted from inventory.");
+                System.out.println("");
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(delete + " is not found.");
+            System.out.println("----------------------------");
+        }
+    }
+    static void prodBuy(){
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+        boolean found = false;
+        System.out.print("Enter product name to buy: ");
+        String bname = s.nextLine();
+        System.out.print("Enter quantity of product: ");
+        int newnum = s1.nextInt();
+        System.out.println("----------------------------");
+        for (int i = 0; i < product.size(); i++) {
+            if (product.get(i).get("pname").toString().equals(bname)){
+                double buyp = (double) product.get(i).get("bp");
+                if (buyp*newnum > balance)
+                    System.out.println("Not enough balance to buy the product.");
+                else{
+                    int pnum = (int) product.get(i).get("anum");
+                    product.get(i).put("anum", pnum + newnum);
+                    balance -= newnum * buyp;
+                    System.out.println(bname + " buy successfully.");
+                }
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(bname + " is not found in product list.");
+            System.out.println("----------------------------");
+        }
+    }
+    static void prodSell(){
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+        boolean found = false;
+        System.out.print("Enter product name to sell: ");
+        String name = s.nextLine();
+        System.out.print("Enter quantity of product: ");
+        int sellnum = s1.nextInt();
+        System.out.println("----------------------------");
+        for (int i = 0; i < product.size(); i++) {
+            if (product.get(i).get("pname").toString().equals(name)){
+                int available = (int) product.get(i).get("anum");
+                if (sellnum > available)
+                    System.out.println("Product is not available.");
+                else{
+                    int pnum1 = (int) product.get(i).get("anum");
+                    double pprofit = (double) product.get(i).get("profit");
+                    double sellp = (double) product.get(i).get("sp");
+                    double buyp = (double) product.get(i).get("bp");
+
+                    product.get(i).put("anum", pnum1 - sellnum);
+                    product.get(i).put("profit", pprofit + (sellp - buyp) * sellnum);
+                    balance += (sellnum * sellp);
+                    System.out.println(name + " sell successfully.");
+                }
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println(name + " is not found in product list.");
+            System.out.println("----------------------------");
+        }
+    }
 
     public static void main(String[] args) {
 
-        List<product> p = new ArrayList<product>();
-        Scanner s = new Scanner(System.in);
-        Scanner s1 = new Scanner(System.in);
+        Scanner data = new Scanner(System.in);
         int ch;
         do{
             System.out.println("1.Add a product");
@@ -61,59 +158,26 @@ public class Main {
             System.out.println("6.See available balance");
             System.out.println("7.Exit");
             System.out.print("Enter Your Choice : ");
-            ch = s.nextInt();
+            ch = data.nextInt();
 
             switch (ch){
                 case 1:
-                    System.out.println("Enter product name: ");
-                    String pname = s1.nextLine();
-                    System.out.println("Enter buy price: ");
-                    double bp = s.nextDouble();
-                    System.out.println("Enter sell price: ");
-                    double sp = s.nextDouble();
-                    System.out.println("Enter available product in the inventory to sell: ");
-                    int aprod = s.nextInt();
-                    System.out.println("Enter total profit selling the product: ");
-                    double profit = s.nextDouble();
-                    p.add(new product(pname, bp, sp, aprod, profit));
+                    prodAdd();
                     break;
                 case 2:
-                    boolean found = false;
-                    System.out.print("Enter your product name to delete :");
-                    String name = s1.nextLine();
-                    System.out.println("----------------------------");
-                    Iterator<product> i = p.iterator();
-                    while(i.hasNext()){
-                        product e = i.next();
-                        if(e.getProdname().equals(name))  {
-                            i.remove();
-                            found = true;
-                        }
-                    }
-                    if(!found){
-                        System.out.println("Your product is not found");
-                    }else{
-                        System.out.println("Your product is deleted successfully");
-                        System.out.println("To see the new product list enter 5");
-                    }
-                    System.out.println("----------------------------");
+                    prodDelete();
+                    break;
+                case 3:
+                    prodBuy();
+                    break;
+                case 4:
+                    prodSell();
                     break;
                 case 5:
-                    System.out.println("-----------------------------------------------------------------------------");
-                    System.out.printf("%10s %20s %10s", "Product Name", "Available Product", "Profit");
-                    System.out.println();
-                    System.out.println("-----------------------------------------------------------------------------");
-                    for(product product: p){
-                        System.out.format("%10s %15d %17.2f",
-                                product.getProdname(), product.getAvailnum(), product.getProfit());
-                        System.out.println();
-                    }
-                    System.out.println("-----------------------------------------------------------------------------");
+                    prodShow();
                     break;
                 case 6:
-                    System.out.println();
-                    System.out.println("Available balance: " + balance + " BDT");
-                    System.out.println();
+                    balance();
                     break;
             }
         }while(ch!=7);
